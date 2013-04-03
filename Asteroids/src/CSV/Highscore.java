139a85f293d9fde9f28cb32cbcd43d77eeef3904
chiwingsit <chi-wing.sit@mail.mcgill.ca>
2013-04-03 00:10:48 -0400

    Changed Highscore.java

--- Asteroids/src/CSV/Highscore.java
+++ Asteroids/src/CSV/Highscore.java
@@ -14,35 +14,35 @@ import au.com.bytecode.opencsv.bean.CsvToBean;
 
 public class Highscore {
 	private static final String HIGHSCORES_FILE="resources/CSV/highscores.csv";
-	
+
 	private String username;
 	private int score;
-	
+
 	public Highscore(){
-		
+
 	}
-	
+
 	public Highscore(String username, int score){
 		this.username = username;
 		this.score = score;
 	}
-	
+
 	public String getUsername(){
 		return username;
 	}
-	
+
 	public void setUsername(String username){
 		this.username = username;
 	}
-	
+
 	public int getScore(){
 		return score;
 	}
-	
+
 	public void setScore(int score){
 		this.score =  score;
 	}
-	
+
 	public static void addScore(String username, int score) throws IOException{
 		ColumnPositionMappingStrategy<Highscore> strat = new ColumnPositionMappingStrategy<Highscore>();
 		strat.setColumnMapping(new String[] {"username","score"});
@@ -51,34 +51,35 @@ public class Highscore {
 		strat.setColumnMapping(new String[] {"username", "score"});
 		CSVReader reader = new CSVReader(new FileReader(HIGHSCORES_FILE));
 		List highscoresList = csv.parse(strat, reader);
-		
+		reader = new CSVReader(new FileReader(HIGHSCORES_FILE));
+		List<String[]> writeHighscores = reader.readAll();
 		for(Object object : highscoresList){
 			Highscore highscore = (Highscore) object;
 			if(highscore.getScore() < score){
 				int index = highscoresList.indexOf(object);
-				highscoresList.add(index, new Highscore(username, score));
-				highscoresList.remove(10);
+				writeHighscores.add(index, new String[] {username, ""+score});
+				writeHighscores.remove(10);
 
 				CSVWriter writer = new CSVWriter(new FileWriter(HIGHSCORES_FILE));
-				writer.writeAll(highscoresList);
+				writer.writeAll(writeHighscores);
 				writer.close();
 				System.out.println("Highscores have been changed");
 
 				break;
 			}
 		}
-		
+
 		reader.close();
 	}
-	
+
 	public static List<String[]> getHighscores() throws FileNotFoundException, IOException{
-		
+
 		CSVReader reader = new CSVReader(new FileReader(HIGHSCORES_FILE));
 		List<String[]> highscores = reader.readAll();
 		reader.close();
-		
+
 		return highscores;
-		
+
 	}
 
 }
