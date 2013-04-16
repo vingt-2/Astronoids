deee36d6bf99aa4e98718ae5c0ac8074d8eede44
sidgandhi <siddharth.gandhi@mail.mcgill.ca>
2013-04-16 15:02:41 -0400

    Added comments to soundeffects enum

--- Asteroids/src/Helpers/SoundEffect.java
+++ Asteroids/src/Helpers/SoundEffect.java
@@ -3,9 +3,17 @@ package Helpers;
 import java.io.*;
 import javax.sound.sampled.*;
 
+/**
+ * SoundEffect:
+ * 		A sound effects enum which encapsulates all sound effects and music:
+ * 			Action related sfx, background music, etc..
+ * 			The sounds are then simply played where events occur by
+ * 			using the command SoundEffect.EFFECTNAME.play()
+ */
 
 public enum SoundEffect
 {
+	//This is where all the different sounds are defined and the filenames specified
 	SHOOT("shoot.wav"),
 	AFTERBURN("afterburn.wav"),
 	CRASH("crash.wav"),
@@ -30,6 +38,8 @@ public enum SoundEffect
 	{
 		try
 		{
+			//loading sound files as clips into the game
+
 			File soundclip = new File("./resources/soundfx/"+soundFileName);
 			
 			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundclip);
@@ -53,6 +63,10 @@ public enum SoundEffect
 
 	}
 	
+	/**
+	 * Method used to play sounds, also rewinds clips by setting frame position to 0 before starting
+	 */
+
 	public void play()
 	{
 		if(volume != Volume.MUTE)
